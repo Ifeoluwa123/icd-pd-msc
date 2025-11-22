2f492d5c6b4b870444de8cc443f7ac44db3c2b8a
adeleye ifeoluwa seun <hipheoluwaseun@gmail.com>
2025-11-22 16:53:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7095287690-da920",
+  "appId": "1:498155772782:web:b6be2b9aef1d7a880e3f7f",
+  "apiKey": "AIzaSyCVf_5vJlTfh4u-2OyRYQ4tVUf0Bz3lZiM",
+  "authDomain": "studio-7095287690-da920.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "498155772782"
+};
