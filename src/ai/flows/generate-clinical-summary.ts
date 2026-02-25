a8a1e4a1fc696e8759540bd69fb9f38aaeb2d6fb
Adeleye Ifeoluwa Seun <35472628+Ifeoluwa123@users.noreply.github.com>
2026-02-25 21:04:01 +0100

    Update generate-clinical-summary.ts

--- src/ai/flows/generate-clinical-summary.ts
+++ src/ai/flows/generate-clinical-summary.ts
@@ -11,14 +11,13 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import {z} from 'genkit';
 
- const ai = genkit({
-  plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),
-  ],
+
+
+const ai = genkit({
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
 });
+
 
 
 
